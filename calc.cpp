265332129fe009527e9378ccccd2d751eb764c51
brenio55 <brenio55@outlook.com>
2023-02-22 22:36:35 -0300

    Adicionado média entre 5 números

--- calc.cpp
+++ calc.cpp
@@ -100,12 +100,42 @@ void raizQuadrada(){
     system("CLS");
 }
 
+void media5(){
+    system("CLS");
+    int number[4];
+    int r, resp;
+
+    cout << "Escolha o primeiro número\n";
+    cout << ">: ";
+    cin >> number[0];
+    cout << "Escolha o segundo número\n";
+    cout << ">: ";
+    cin >> number[1];
+    cout << "Escolha o terceiro número\n";
+    cout << ">: ";
+    cin >> number[2];
+    cout << "Escolha o quarto número\n";
+    cout << ">: ";
+    cin >> number[3];
+    cout << "Escolha o quinto número\n";
+    cout << ">: ";
+    cin >> number[4];
+    
+    r = (number[0] + number[1] + number[2] + number[3] + number[4]) / 5;
+
+    cout << "O resultado da média entre os 5 números digitados é igual a: " << r << endl;
+    cout << "Digite 0 para voltar ao menu inicial.\n";
+    cout << ">: ";
+    cin >> resp;
+    system("CLS");
+}
+
 void menuOpcao(){
     int valorEntrada = NULL; //NULL é um valor de inicialização da variável que significa VAZIO. É equivalente ao 0.
     
     while (valorEntrada == NULL){  
         cout << "Escolha a opção desejada: \n";
-        cout << "1: Adição\n2: Multiplicação\n3: Divisão\n4: Subtração\n5: Raiz quadrada\n6: Fechar programa\n";
+        cout << "1: Adição\n2: Multiplicação\n3: Divisão\n4: Subtração\n5: Raiz quadrada\n6: Média entre 5 números\n7: Sair do programa\n";
         cout << ">: ";
         cin >> valorEntrada;
 
@@ -130,7 +160,11 @@ void menuOpcao(){
                     valorEntrada = NULL;
             break;
 
-            case 6: exit(EXIT_SUCCESS);
+            case 6: media5();
+                    valorEntrada = NULL;
+            break;
+
+            case 7: exit(EXIT_SUCCESS);
             break;
 
             default:
